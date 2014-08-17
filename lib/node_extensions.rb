46b57934eb6ebdceec5ea74d82b117e78f672e69
AlexanderTachciev <alexander.tahchiev@gmail.com>
2014-08-17 17:39:45 +0300

    Add module Lisputin everywhere

--- lib/node_extensions.rb
+++ lib/node_extensions.rb
@@ -1,22 +1,24 @@
-module Sexp
-  class Program < Treetop::Runtime::SyntaxNode
-  end
+module Lisputin
+  module Sexp
+    class Program < Treetop::Runtime::SyntaxNode
+    end
 
-  class ListLiteral < Treetop::Runtime::SyntaxNode
-  end
+    class ListLiteral < Treetop::Runtime::SyntaxNode
+    end
 
-  class IntegerLiteral < Treetop::Runtime::SyntaxNode
-  end
-  
-  class StringLiteral < Treetop::Runtime::SyntaxNode
-  end
-  
-  class FloatLiteral < Treetop::Runtime::SyntaxNode
-  end
-  
-  class IdentifierLiteral < Treetop::Runtime::SyntaxNode
-  end
-  
-  class Body < Treetop::Runtime::SyntaxNode
+    class IntegerLiteral < Treetop::Runtime::SyntaxNode
+    end
+    
+    class StringLiteral < Treetop::Runtime::SyntaxNode
+    end
+    
+    class FloatLiteral < Treetop::Runtime::SyntaxNode
+    end
+    
+    class IdentifierLiteral < Treetop::Runtime::SyntaxNode
+    end
+    
+    class Body < Treetop::Runtime::SyntaxNode
+    end
   end
 end
--- lib/parser.rb
+++ lib/parser.rb
@@ -6,25 +6,27 @@ BASE_PATH = File.expand_path(File.dirname(__FILE__))
 # Load our custom syntax node classes so the parser can use them
 require File.join(BASE_PATH, 'node_extensions.rb')
 
-class Parser
-  
-  # Load the Treetop grammar from the 'sexp_parser' file, and 
-  # create a new instance of that parser as a class variable 
-  # so we don't have to re-create it every time we need to 
-  # parse a string
-  Treetop.load(File.join(BASE_PATH, 'lisputin.treetop'))
-  @@parser = SexpParser.new
-  
-  def self.parse(data)
-    # Pass the data over to the parser instance
-    tree = @@parser.parse(data)
+module Lisputin
+  class Parser
     
-    # If the AST is nil then there was an error during parsing
-    # we need to report a simple error message to help the user
-    if(tree.nil?)
-      raise Exception, "Parse error at offset: #{@@parser.index}"
+    # Load the Treetop grammar from the 'sexp_parser' file, and 
+    # create a new instance of that parser as a class variable 
+    # so we don't have to re-create it every time we need to 
+    # parse a string
+    Treetop.load(File.join(BASE_PATH, 'lisputin.treetop'))
+    @@parser = SexpParser.new
+    
+    def self.parse(data)
+      # Pass the data over to the parser instance
+      tree = @@parser.parse(data)
+      
+      # If the AST is nil then there was an error during parsing
+      # we need to report a simple error message to help the user
+      if(tree.nil?)
+        raise Exception, "Parse error at offset: #{@@parser.index}"
+      end
+      
+      return tree
     end
-    
-    return tree
   end
 end
